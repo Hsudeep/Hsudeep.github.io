a779ccde1665fc169d762553e9a5c87193a0c603
Hsudeep <haldarsudeep28@gmail.com>
2022-08-19 12:51:59 +0530

    deploying_portfolio

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import Home from "./views/Home";
+import Projects from "./views/Projects";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+  return (
+    <>
+      {!loading ? (
+        <div>
+          <Navbar />
+          <Home/>
+          <About/>
+          <Projects/>
+          <Contact/>
+        </div>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,134 @@
+import skin_Store from "./assets/skinstore.png";
+import shop_com from "./assets/shopcom-logo-svg_file.svg";
+import mode_sens from "./assets/modeSens_2.png";
+export const techStack = [
+  {
+    name: "HTML",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+  },
+  {
+    name: "Javascript",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "NodeJS",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+  {
+    name: "React",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+  },
+  {
+    name: "TypeScript",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "Express",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+  },
+  {
+    name: "Redux",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+  },
+  {
+    name: "Postman",
+    link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+  },
+  {
+    name: "Mongo DB",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
+  },
+  {
+    name: "Heroku",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg",
+  },
+  {
+    name: "Git",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+  },
+];
+
+export const contactLinks = [
+  {
+    name: "Twitter",
+    url: "https://img.icons8.com/doodle/40/000000/twitter-squared.png",
+    link: "https://twitter.com/H_Sudeep28",
+  },
+  {
+    name: "Instagram",
+    url: "https://img.icons8.com/doodle/40/000000/instagram-new--v2.png",
+    link: "https://www.instagram.com/sudeep_028/",
+  },
+  {
+    name: "Github",
+    url: "https://img.icons8.com/doodle/40/000000/github--v1.png",
+    link: "https://github.com/Hsudeep",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://img.icons8.com/doodle/40/000000/linkedin--v2.png",
+    link: "https://www.linkedin.com/in/hsudeep028/",
+  },
+];
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Shop.com Clone",
+    img: shop_com,
+    desc: "SHOP.COM is a new kind of shopping comparison site that offers the most comprehensive shopping experience on the web. Our shopping experts have drawn on their industry experience and insider connections to bring you the best collection of the stores you can't live without, and the brands and products you love - all in one place.",
+    github:
+      "https://github.com/Hsudeep/shop.com?organization=Hsudeep&organization=Hsudeep",
+    link: "https://stirring-babka-e9aa79.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "ModeSens",
+    img: mode_sens,
+    desc: "ModeSens is your digital shopping assistant for the smart and informed. At ModeSens, we believe in the beauty of discovery, and we aim to create a community of like-minded individuals who are inspired by fashion and driven by technology, just like us.",
+    github:
+      "https://github.com/Hsudeep/Modesens?organization=Hsudeep&organization=Hsudeep",
+    link: "https://magnificent-bublanina-64e24e.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "SkinStore",
+    img: skin_Store,
+    desc: "For over 24 years skistore has been your number 1 destination for premium beauty, delivering the latest in innovative clinical skincare and luxury spa products. SkinStore was one of the first online stores to offer dermatologist-created and recommended products worldwide.",
+    github: "https://github.com/Hsudeep/little-spark-7917",
+    link: "https://celebrated-chaja-01937d.netlify.app/",
+  },
+];
+export const serviceData = [
+  {
+    name: "Web Application Development",
+    desc: "Web design encompasses many different skills and disciplines in the production and maintenance of websites. The different areas of web design include web graphic design user interface design  authoring, including standardised code and proprietary software user experience design and search engine",
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png",
+  },
+  {
+    name: "Mobile Application Development",
+    desc: `We provide a range of mobile application development services
+    including custom mobile development on Android platforms, building
+    cross-platform apps, designing user experience and integrating
+    novel mobile interfaces such as chat and voice`,
+    img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png",
+  },
+
+  {
+    name: "Backend Development",
+    desc: `Graphic design is a craft where professionals create visual
+    content to communicate messages. By applying visual hierarchy and
+    page layout techniques, designers use typography and pictures to
+    meet users' specific needs and focus on the logic of displaying
+    elements in interactive designs, to optimize the user experience.`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png",
+  },
+];
